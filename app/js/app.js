2f66e9a3d0fe6fff5a4a7436a8c6d3ee6a0be8fa
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-05-27 21:11:30 +0300

    baseServerUrl fix

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,5 @@
 
 var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap.pagination']);
 
-app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net');
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net');
 app.constant('pageSize', 5);
