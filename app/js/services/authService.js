a946d906fcc98fb90687c97020824ab8d9dabb5b
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-05-27 20:50:25 +0300

    authService.js

---
+++ app/js/services/authService.js
@@ -0,0 +1,44 @@
+'use strict';
+
+app.factory('authService',
+    function ($http, baseServiceUrl) {
+        return {
+            login: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/login',
+                    data: userData
+                };
+                $http(request).success(function(data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            register: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/users/register',
+                    data: userData
+                };
+                $http(request).success(function(data) {
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            logout: function() {
+                delete sessionStorage['currentUser'];
+            },
+
+            getAuthHeaders : function() {
+                var headers = {};
+                var currentUser = this.getCurrentUser();
+                if (currentUser) {
+                    headers['Authorization'] = 'Bearer ' + currentUser.access_token;
+                }
+                return headers;
+            }
+        };
+    }
+);
