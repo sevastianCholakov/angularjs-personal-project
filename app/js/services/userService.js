b524f019be89ca50eb7ea4ae864bc872bdb77c41
sevastianCholakov <sevastian.cholakov@gmail.com>
2015-05-29 13:28:37 +0300

    sendFriendRequest(),approveFriendRequest(),rejectFriendRequest()

--- app/js/services/userService.js
+++ app/js/services/userService.js
@@ -11,7 +11,8 @@ app.factory('userService',
                 };
                 $http(request).success(success).error(error);
             },
-             getUserFullData: function (user, success, error) {
+
+            getUserFullData: function (user, success, error) {
                 var request = {
                     method: 'GET',
                     url: baseServiceUrl + '/api/users/' + user,
@@ -27,7 +28,54 @@ app.factory('userService',
                     headers: authService.getAuthHeaders()
                 };
                 $http(request).success(success).error(error);
+            },
+
+            editProfile: function (data, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/me',
+                    headers: authService.getAuthHeaders(),
+                    data: data
+                };
+                $http(request).success(success).error(error);
+            },
+
+            changePassword: function (data, success, error) {
+                var request = {
+                    method: 'PUT',
+                    url: baseServiceUrl + '/api/me/changepassword',
+                    headers: authService.getAuthHeaders(),
+                    data: data
+                };
+                $http(request).success(success).error(error);
+            },
+            //  Friends part
+            sendFriendRequest: function (user, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/me/requests/' + user,
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            approveFriendRequest: function (requestId, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/me/requests/' + requestId + '?status=approved',
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
+            },
+
+            rejectFriendRequest: function (requestId, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/me/requests/' + requestId + '?status=rejected',
+                    headers: authService.getAuthHeaders()
+                };
+                $http(request).success(success).error(error);
             }
         };
     }
-);
+    );
